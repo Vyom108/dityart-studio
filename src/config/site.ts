ee2d6749f6911fc6ea88fffbb1ffbb7de6435b9e
dreamer_ demate <vyomsejpal6@gmail.com>
2026-08-31 17:05:09 +0530

    Your change description

--- src/config/site.ts
+++ src/config/site.ts
@@ -17,7 +17,7 @@ export const site = {
     email: "dityartstudio@gmail.com",
     whatsapp: "919081913600",
     instagram: "https://instagram.com/dityart_studio",
-    facebook: "https://facebook.com/dityartstudio",
+    facebook: "https://facebook.com/DityArt Studio",
   },
   hours: {
     label: "Studio hours",
